31073bcc5c2bb060016e79dab746dffeed5221a8
Jun Young Yoon <jjun9288@gmail.com>
2021-11-19 09:37:17 +0900

    평균값 필터

---
+++ ch4/1.blurring1.py
@@ -0,0 +1,33 @@
+import sys
+import numpy as np
+import cv2
+path = 'C:/Users/jjun8/Desktop/CV_study/ch4/data/'
+
+
+#-----------------------------------------------------------------------------------------
+# < 블러링 >
+# - 평균 값 필터 : 영상의 특정 좌표 값들의 평균으로 설정, 픽셀들 간 grayscale 값 변화가
+#                  줄어들어 날카로운 edge가 무뎌진다. 
+# cv2.blur(src, ksize, dst=None, anchor=None, borderType=None)
+#-----------------------------------------------------------------------------------------
+
+
+
+src = cv2.imread(path + 'rose.bmp', cv2.IMREAD_GRAYSCALE)
+
+if src is None:
+    print('Image load failed!')
+    sys.exit()
+
+
+dst = cv2.blur(src, (3, 3))
+
+#다른 방법)
+#kernel = np.ones((3, 3), dtype=np.float64) / 9.
+#dst = cv2.filter2D(src, -1, kernel)
+
+cv2.imshow('src', src)
+cv2.imshow('dst', dst)
+cv2.waitKey()
+
+cv2.destroyAllWindows()
