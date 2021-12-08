8795a7565cdf8deb9cd740402ce253779284d656
Jun Young Yoon <jjun9288@gmail.com>
2021-12-08 09:52:15 +0900

    영상의 이진화

--- ch7/1.threshold1.py
+++ ch7/1.threshold1.py
@@ -6,6 +6,11 @@ path = 'C:/Users/jjun8/Desktop/CV_study/ch7/data/'
 
 #-----------------------------------------------------------------------------------------------------------
 # < 영상의 이진화 >
+# 영상의 픽셀 값을 0 또는 255로 만드는 연산
+# cv2.threshold(src, thresh, maxval, type)
+#   -thresh : 사용자 지정 임계값
+#   -maxval : cv2.THRESH_BINARY 또는 cv2.THRESH_BINARY_INV 방법 사용 시 최댓값. 보통 255로 지정
+#   -type : cv2.THRESH_로 시작하는 플래그
 #-----------------------------------------------------------------------------------------------------------
 
 
@@ -16,7 +21,7 @@ if src is None:
     print('Image load failed!')
     sys.exit()
 
-_, dst1 = cv2.threshold(src, 100, 255, cv2.THRESH_BINARY)
+_, dst1 = cv2.threshold(src, 100, 255, cv2.THRESH_BINARY)   # _은 뒤에서 배울 otsu
 _, dst2 = cv2.threshold(src, 210, 255, cv2.THRESH_BINARY)
 
 cv2.imshow('src', src)
